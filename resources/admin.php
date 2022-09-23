1a3c7310439a515da0dd7e00d2dce4579c1cd564
Ademola Quozeem <72305068+Quozeem@users.noreply.github.com>
2022-09-23 18:34:36 +0100

    Update admin.php

--- resources/admin.php
+++ resources/admin.php
@@ -35,51 +35,7 @@ Route::get('/pages/edit', 'PizzaController@edit');
 Route::put('/pages/{edit}', 'PizzaController@UPDATES');
 Route::get('/pages/updateid/{ids}', 'PizzaController@updateid');
 Route::get('/pages/update', 'PizzaController@update');
-Route::get('/pages/create', 'PizzaController@create');
-
-// Admin route
-//Route::prefix('admin')->name('admin.')->group(function(){
- // Route::resource('/index','App\Http\Controllers\PizzaController@destroy');
-//});
-
-Route::get('/jointable','PizzaController@jointable')->middleware('member')->name('jointable');
-//Route::get('/logout','App\Http\Controllers\Auth\LoginController@logouts')->name('logout');
-Route::put('/UPDATE/{id}','PizzaController@updatedash')->middleware('member');
-Route::get('/Admin','PizzaController@Admin')->middleware('admin')->name('admin');
-Route::get('/dashboardedit/{id}','PizzaController@editid')->middleware('member')->name('dashboardedit');
-Route::get('/dashboard','PizzaController@adminfetch')->middleware('member')->name('dashboard');//
-Route::get('/delete/{id}','PizzaController@delid')->middleware('member')->name('delete');
-Route::get('/edit/{id}','PizzaController@editid')->middleware('member')->name('edit');
-//Route::get('/dashboard',function () {
- // $selected=Pizza::latest()->get(); 
- // return view('dashboard',['select' => $selected]);
- //Route::group(['prefix'=> 'member','namespace'=> 'Auth'],function(){
-
-  Route::post('/pages/login', 'Auth\LoginController@login');
-  Route::post('/login', 'Auth\LoginController@loginuser')->name('login');
-  Route::get('/pages/login', 'Auth\LoginController@showlogin')->name('pages.login');
-  Route::get('/home', 'Auth\LoginController@index')->name('home');
- //});
-//})->middleware('admin')->name('dashboard');
-Auth::routes();
-
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-//Route::group(['middleware'=>['auth']],function(){
-  Route::get('/home',function () {
-   
-    return view('home');
-   
-  })->middleware('auth')->name('home');
-  
-  
-
-//Route::group(['middleware' => 'auth:admin'], function () {Auth::routes();Route::view('/dashboard', 'dashboard');});
-//Route::group(['middleware' => 'auth:admin'], function () {
-    
-  //Route::view('/dashboard', 'dashboard');
-//});
-
+Route::get('/pages/create', 'PizzaController@
  //Route::middleware(['auth','isAdmin'])->group(function(){
    // Route::get('/admin',function(){
      ///   return view('admin.dashboard');
